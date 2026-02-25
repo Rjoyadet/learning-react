0d14ee39ef807f12c3f9e7ef8d3fe4acd2a708e7
Rjoyadet <57276368+Rjoyadet@users.noreply.github.com>
2026-02-24 17:01:57 -0700

    Add files via upload
    uploaded the rest of the folders

---
+++ components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+
+function NavBar() {
+    return <nav className="navbar">
+<div className="navbar-brand">
+
+    <Link to="/">Movie App</Link>
+</div>
+<div className="navbar-links"></div>
+<Link to="/" className="nav-link">Home</Link>
+<Link to="/favorites" className="nav-link">Favorites</Link>
+    </nav>
+
+}
+
+export default  NavBar
